6baf77012eaf62350af7a6eb5f934325a31b107d
Silvio Traversaro <silvio@traversaro.it>
2024-03-28 17:31:35 +0100

    Update run_test.bat

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -5,7 +5,7 @@ mkdir build
 
 :: Compile example that links imgui
 cd build
-cmake -GNinja -DCMAKE_BUILD_TYPE=Release ..
+cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%CONDA_PREFIX%\Library ..
 if errorlevel 1 exit 1
 
 cmake --build . --config Release
