a00518b54cefba5dff36a4b7dd975ee1b6c25646
Silvio Traversaro <silvio@traversaro.it>
2024-03-29 09:52:24 +0100

    Cleanup test CMake

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -5,7 +5,7 @@ mkdir build
 
 :: Compile example that links imgui
 cd build
-cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%CONDA_PREFIX%\Library ..
+cmake -GNinja -DCMAKE_BUILD_TYPE=Release ..
 if errorlevel 1 exit 1
 
 cmake --build . --config Release
